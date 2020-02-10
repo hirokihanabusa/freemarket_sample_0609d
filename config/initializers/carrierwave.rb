f9267b52b360d1a850b41dc83949dce61b52597e
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-02-10 20:03:33 +0900

    デプロイエラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory = 'freemarketsample0609d'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample0609d'
+  config.fog_directory = 'freemarket-sample-0609d'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-0609d'
 end
