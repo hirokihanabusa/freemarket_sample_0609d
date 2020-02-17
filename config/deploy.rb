8ee263c7799b52d56722ddaaebfe7459cbbf7d2e
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-02-17 14:03:27 +0900

    デプロイエラー修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,23 @@
-lock '3.11.0'
+lock "3.11.0"
 
-set :application, 'freemarket_sample_0609d'
+set :log_level, :debug
+set :application, "freemarket_sample_0609d"
+set :repo_url, "git@github.com:Riku-O/freemarket_sample_0609d.git"
 
-set :repo_url,  'git@github.com:Atsushi-Kuwabara/freemarket_sample_0609d.git'
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
-set :linked_dirs, fetch(:linked_dirs, []).push('config/master.key', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatspace.pem'] 
+    keys: ['/Users/tanakakenyou/.ssh/freemarket_sample_0609d04.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
@@ -44,3 +25,17 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :upload do
+    on roles(:app) do |host|
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
