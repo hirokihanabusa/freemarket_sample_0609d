137d2e0a746ceaa0933b33d58b1872369682e927
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-02-17 19:34:37 +0900

    デプロイエラー修正

--- Gemfile
+++ Gemfile
@@ -74,7 +74,6 @@ group :production do
   gem 'unicorn', '5.4.1'
 end
 
-
 gem 'haml-rails'
 gem 'font-awesome-rails'
 gem 'carrierwave'
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock "3.11.0"
 
 set :log_level, :debug
 set :application, "freemarket_sample_0609d"
-set :repo_url, "git@github.com:Riku-O/freemarket_sample_0609d.git"
+set :repo_url, "git@github.com:Atsushi-Kuwabara/freemarket_sample_0609d.git"
 
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
@@ -12,7 +12,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-    keys: ['/Users/tanakakenyou/.ssh/freemarket_sample_0609d04.pem']
+    keys: ['/Users/atsushikuwabara/.ssh/chatspace.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
