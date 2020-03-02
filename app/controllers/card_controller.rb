669ce6463dcfefeac48064dd61e5d209e6f31600
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-03-02 17:52:29 +0900

    payjp修正

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -9,7 +9,7 @@ class CardController < ApplicationController
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
-    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+    Payjp.api_key = Rails.application.credentials.payjp(:PAYJP_SECRET_KEY)
 
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -56,7 +56,7 @@ class CardController < ApplicationController
     if card.blank?
       redirect_to action: "new"
     else
-      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      Payjp.api_key = Rails.application.credentials.payjp(:PAYJP_SECRET_KEY)
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
